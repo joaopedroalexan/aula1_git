2397510db0f5e8f6b5791aa57f9132d43c3b0524
joaopedroalexan <joaopedroalexsilva@gmail.com>
2024-05-27 11:24:39 -0300

    lista

---
+++ python/lista.py
@@ -0,0 +1,29 @@
+import time
+
+numbers = [111, 7, 2, 1]
+
+print(len(numbers))
+
+print(numbers)
+
+numbers.append (52)
+
+print(len(numbers))
+
+print(numbers)
+
+numbers.insert (0, 222)
+
+print(len (numbers))
+print(numbers)
+
+numbers.insert(-1, 12)
+print(numbers)
+
+numbers.insert(1, 18)
+print(numbers)
+
+numbers.insert(-2, 20)
+print(numbers)
+
+time.sleep(3)
